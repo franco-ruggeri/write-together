ba6118a7bd133a0c6c8e6ee2cd2b11ae612f66f9
Franco Ruggeri <fruggeri@kth.se>
2020-08-17 19:10:14 +0200

    fix Socket, now working for multiple messages arriving in a single TCP segment

--- src/network/Socket.cpp
+++ src/network/Socket.cpp
@@ -6,31 +6,34 @@
 
 namespace cte {
     Socket::Socket(){
-        connect(this, &Socket::readyRead, this, &Socket::check_message_ready);
+        connect(this, &Socket::readyRead, this, &Socket::prepare_messages);
     }
     Socket::Socket(int socket_fd) {
         if (!setSocketDescriptor(socket_fd))
             throw std::runtime_error("setSocketDescriptor() failed");
-        connect(this, &Socket::readyRead, this, &Socket::check_message_ready);
+        connect(this, &Socket::readyRead, this, &Socket::prepare_messages);
     }
 
-    void Socket::check_message_ready() {
-        if (canReadLine()) emit ready_message();
+    void Socket::prepare_messages() {
+        while (canReadLine()) {
+            // read line
+            QByteArray bytes = readLine();
+            if (bytes.at(bytes.size()-1) != '\n')
+                throw std::runtime_error("read() failed");
+
+            // remove '\n' and, if present, '\r'
+            bytes.remove(bytes.size()-1, 1);
+            if (bytes.at(bytes.size()-1) == '\r')
+                bytes.remove(bytes.size()-1, 1);
+
+            // add message to buffer
+            messages_.enqueue(Message::deserialize(bytes));
+            emit ready_message();
+        }
     }
 
     QSharedPointer<Message> Socket::read_message() {
-        QByteArray bytes = readLine();
-
-        // check result
-        if (bytes.at(bytes.size()-1) != '\n')
-            throw std::runtime_error("read() failed");
-
-        // remove '\n' and, if present, '\r'
-        bytes.remove(bytes.size()-1, 1);
-        if (bytes.at(bytes.size()-1) == '\r')
-            bytes.remove(bytes.size()-1, 1);
-
-        return Message::deserialize(bytes);
+        return messages_.dequeue();
     }
 
     void Socket::write_message(const QSharedPointer<Message>& message) {
