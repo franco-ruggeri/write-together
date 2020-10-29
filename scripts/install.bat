5aeb101bb5297f06a8e7af5b7efd997f2cebf567
sdiblasio <stefano.diblasio@studenti.polito.it>
2020-10-29 22:47:49 +0100

    fix paths in installer for windows

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,13 +2,15 @@
 echo Set oWS = WScript.CreateObject("WScript.Shell") > ClientShortcut.vbs
 echo If not oWS Is Nothing Then >> ClientShortcut.vbs
 echo DesktopPath = oWS.SpecialFolders("Desktop") >> ClientShortcut.vbs
-echo Set oLink = oWS.CreateShortcut(DesktopPath ^& "\Collaborative Doc.lnk") >> ClientShortcut.vbs
-echo oLink.Arguments = "localhost 8081" >> ClientShortcut.vbs
-echo oLink.Description = "Collaborative Text Editor" >> ClientShortcut.vbs
-echo oLink.IconLocation = "%CD%\src\widget\ui\images\app\logo.ico" >> ClientShortcut.vbs
-echo oLink.TargetPath = "%CD%\cmake-build-debug\src\client_new\client.exe" >> ClientShortcut.vbs & :: to be updated
+echo Set oFS = WScript.CreateObject("Scripting.FileSystemObject") >> ClientShortcut.vbs
+echo rootPath = oFS.GetParentFolderName(oFS.GetParentFolderName(WScript.ScriptFullName)) >> ClientShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(DesktopPath ^& "\Real-time Collaborative Text Editor.lnk") >> ClientShortcut.vbs
+echo oLink.Arguments = "localhost 5000" >> ClientShortcut.vbs
+echo oLink.Description = "Real-time Collaborative Text Editor" >> ClientShortcut.vbs
+echo oLink.IconLocation = rootPath ^& "\src\client\ui\images\app\logo_black.ico" >> ClientShortcut.vbs
+echo oLink.TargetPath = rootPath ^& "\cmake-build-debug\src\client\client.exe" >> ClientShortcut.vbs & :: to be updated
 echo oLink.WindowStyle = 1 >> ClientShortcut.vbs & :: normal, not maximized
-echo oLink.WorkingDirectory = "%CD%" >> ClientShortcut.vbs
+echo oLink.WorkingDirectory = "%CD%\..\" >> ClientShortcut.vbs
 echo oLink.Save >> ClientShortcut.vbs
 echo end if >> ClientShortcut.vbs
 cscript ClientShortcut.vbs /NoLogo /T:10 /D
